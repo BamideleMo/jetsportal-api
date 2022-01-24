fa56b691f10d569eab47906bba7e13b6ebda858a
bamidelemo <bamidelemo@gmail.com>
2022-01-24 15:02:23 +0100

    ...

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,4 @@
 export FLASK_ENV=development
 export FLASK_APP=src
+export DATABASE_URL=postgresql://zfurpumvxspiup:95ac8f9c3a1dad58f564e3128b2965f67e13d0adb695584758f3fd3d1e214a83@ec2-3-232-22-121.compute-1.amazonaws.com:5432/d7qk5pfv7ia515
+export JWT_SECRET_KEY='Foluke89'
--- src/__init__.py
+++ src/__init__.py
@@ -16,10 +16,18 @@ def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
     CORS(app)
 
+    my_env = os.getenv("FLASK_ENV")
+    if(my_env=="development"):
+        uri = "postgresql://postgres:Foluke89@localhost/jetsv3"
+    else:
+        uri = os.getenv("DATABASE_URL")
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+
     if test_config is None:
         app.config.from_mapping(
             SECRET_KEY = os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL"),
+            SQLALCHEMY_DATABASE_URI = uri,
             SQLALCHEMY_TRACK_MODIFICATIONS = False,
             JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY"),
         )
