ddabaf45b1c7e15710da013801903cb1c2b22a04
bamidelemo <bamidelemo@penguin>
2022-05-25 17:18:56 +0100

    .

--- src/admin.py
+++ src/admin.py
@@ -354,13 +354,14 @@ def for_receipt_issue():
     data=[]
 
     for a_student in all_students:
+        person = User.query.filter(db.and_(User.username==a_student.student_id))
         data.append({
             'id': a_student.id,
             'student_id': a_student.student_id,
             'ledger_no': a_student.ledger_no,
-            # 'first_name': a_student.first_name,
-            # 'middle_name': a_student.middle_name,
-            # 'last_name': a_student.last_name,
+            'first_name': person.first_name,
+            'middle_name': person.middle_name,
+            'last_name': person.last_name,
         })
     
     return jsonify({
