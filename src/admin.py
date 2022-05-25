2a3c7200d5798178f3559373a6eb09b0b771fde3
bamidelemo <bamidelemo@penguin>
2022-05-25 17:26:58 +0100

    .

--- src/admin.py
+++ src/admin.py
@@ -355,14 +355,16 @@ def for_receipt_issue():
 
     for a_student in all_students:
         person = User.query.filter(db.and_(User.username==a_student.student_id))
-        data.append({
-            'id': a_student.id,
-            'student_id': a_student.student_id,
-            'ledger_no': a_student.ledger_no,
-            'first_name': person.first_name,
-            'middle_name': person.middle_name,
-            'last_name': person.last_name,
-        })
+        if person:
+            # wallet_balance = Registration.query.filter(db.and_(Registration.student_id==a_student.student_id))
+            data.append({
+                'id': a_student.id,
+                'student_id': a_student.student_id,
+                'ledger_no': a_student.ledger_no,
+                'first_name': person.first_name,
+                'middle_name': person.middle_name,
+                'last_name': person.last_name,
+            })
     
     return jsonify({
         "students": data,
