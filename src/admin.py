f6bdba1377c27d2bd499b8954922ca52fdd6123b
bamidelemo <bamidelemo@penguin>
2022-05-26 02:46:01 +0100

    .

--- src/admin.py
+++ src/admin.py
@@ -374,12 +374,12 @@ def for_receipt_issue():
 def get_wallet_balance():
     student_id = request.args
 
-    one_user = Wallet.query.filter(db.and_(Wallet.student_id==student_id)).first()
+    # one_user = Wallet.query.filter(db.and_(Wallet.student_id==student_id)).first()
 
     return jsonify({
-        # "student_id": one_user.student_id,
+        "student_id": student_id,
         # "status": one_user.status,
-        "balance": one_user.amount,
+        # "balance": one_user.amount,
     }), HTTP_200_OK
 
 
