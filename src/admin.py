6c8265c17adca35b3d2f0c6783d39637ceb6c433
bamidelemo <bamidelemo@penguin>
2022-05-28 05:59:02 +0100

    .

--- src/admin.py
+++ src/admin.py
@@ -638,7 +638,7 @@ def fix():
     a_student3.student_id = '68598'
     a_student4.student_id = '68598'
     a_student5.student_id = '68598'
-    a_student6.student_id = '68598'
+    # a_student6.student_id = '68598'
     db.session.commit()
             # status1 = User.query.filter(db.and_(User.username==a_student.student_id)).first()
             # print(status1)
