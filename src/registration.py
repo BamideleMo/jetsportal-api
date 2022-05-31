ed16012c0b7e6012b2372b3e6f7a2dc96055e678
bamidelemo <bamidelemo@penguin>
2022-05-31 03:57:03 +0100

    .

--- src/registration.py
+++ src/registration.py
@@ -500,12 +500,14 @@ def finish_registration():
 @registration.post('/get-lecturer')
 # @jwt_required()
 def get_course_lecturer():
-    pid = request.args.get('period')
-    course_code = request.args.get('code')
 
-    if course_code is None or pid is None:
+    if request.args.get('period') is None:
         pass
     else:
+
+        pid = request.args.get('period')
+        course_code = request.args.get('code')
+        
         period = Period.query.filter(db.and_(Period.id==pid)).first()
         
         one_lecturer_query = Allocatedcourses.query.filter(db.and_(Allocatedcourses.semester==period.semester,
