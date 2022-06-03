5ade8042a9225cda13d53e275ad6e1a58aa406da
bamidelemo <bamidelemo@penguin>
2022-06-03 13:01:19 +0100

    .

--- src/registration.py
+++ src/registration.py
@@ -501,7 +501,7 @@ def finish_registration():
 
     one_user_query = Registration.query.filter(db.and_(Registration.student_id==student_id,Registration.semester==semester,Registration.session==session,Registration.season==season)).first()
     
-    max_id = Registration.query.filter().order_by(Registration.finished_id.desc()).first()
+    max_id = Registration.query.filter(db.and_(Registration.status=='complete')).order_by(Registration.finished_id.desc()).first()
     
     # if isinstance(max_id, int):
     #     finished_id = max_id.finished_id + 1
