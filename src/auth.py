af1f2c0f8278ef47e8a48d4d4d56d056960b56fe
bamidelemo <bamidelemo@penguin>
2022-06-07 14:11:46 +0100

    .

--- src/auth.py
+++ src/auth.py
@@ -30,8 +30,8 @@ def create_profile():
 
         print(programme_category)
 
-        if Student.query.filter_by(phone_number=phone_number).first() is not None:
-            return jsonify({'error':"User already exist."}), HTTP_409_CONFLICT
+        # if Student.query.filter_by(phone_number=phone_number).first() is not None:
+        #     return jsonify({'error':"User already exist."}), HTTP_409_CONFLICT
 
         if Student.query.filter_by(ledger_no=ledger_no).first() is not None:
             return jsonify({'error':"User already exist."}), HTTP_409_CONFLICT
