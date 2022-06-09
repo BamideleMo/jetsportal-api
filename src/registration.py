0deebd1c86db1a37615bc5e72aa33b14bb3ba81d
bamidelemo <bamidelemo@penguin>
2022-06-09 01:06:49 +0100

    .

--- src/registration.py
+++ src/registration.py
@@ -605,7 +605,7 @@ def remove_a_course():
 # @jwt_required()
 def drop_a_course():
     student_id = request.json['student_id']
-    course_code = request.json['course_code']
+    course_code = request.json.get('course_code')
     semester = request.json['semester']
     session = request.json['session']
     season = request.json['season']
