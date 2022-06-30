b2ac8a144726d5e59de12557112dbdf12b33b6c7
bamidelemo <bamidelemo@penguin>
2022-06-30 08:21:59 +0100

    modified to include Add/Drop courses

--- src/faculty.py
+++ src/faculty.py
@@ -144,7 +144,7 @@ def get_class_list():
         )).first()
         if dropped_course:
             # don't append becuase it was dropped
-            print(Droppedcourses.student_id)
+            print(course.student_id)
             pass
         else:
             count = count + 1
