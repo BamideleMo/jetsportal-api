c9e3848c779dcd384359b5fee0ec6cf381da1d8b
bamidelemo <bamidelemo@penguin>
2022-07-19 18:12:19 +0100

    .

--- src/admin.py
+++ src/admin.py
@@ -616,7 +616,7 @@ def get_all_receipts():
     for student_receipt in student_receipts:
         a_user = User.query.filter(User.username == student_receipt.student_id ).first()
         the_student = Student.query.filter(Student.student_id == student_receipt.student_id ).first()
-        print(student_receipt.student_id)
+        print(student_receipt.id)
         data.append({
             'id': student_receipt.id,
             'item': student_receipt.item,
