e6a2af17864d950bbfe5f332ad48059c7d6d470d
bamidelemo <bamidelemo@penguin>
2022-08-04 10:00:14 +0100

    .

--- src/registration.py
+++ src/registration.py
@@ -383,6 +383,7 @@ def get_picked_courses():
                 cost_per_hour_query = Costperhour.query.filter(db.and_(Costperhour.denomination == 'Non-ECWA',Costperhour.level == 'PG',Costperhour.semester == period_query.semester,Costperhour.session == period_query.session,Costperhour.season == period_query.season)).first()
 
             print('ZZZZZZZZZZZZZZZZZZZZ')
+            print(studentid)
             print(course_info.code)
             if(course_info.hours == 'P/F'):
                 hours = 1
