ae33e6358b1db0dc9ce55d9964549c0096aa2ba8
Bamidele Moses <62689539+BamideleMo@users.noreply.github.com>
2023-02-20 17:43:04 +0100

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -33,7 +33,7 @@ def create_profile():
             return jsonify({'error':"User already exist."}), HTTP_409_CONFLICT
 
         if User.query.filter_by(username=username).first() is not None:
-            return jsonify({'error':"User already exist."}), HTTP_409_CONFLICT
+            return jsonify({'error':"Duplicate user."}), HTTP_409_CONFLICT
 
         # if Ledgernumbers.query.filter(db.and_(Ledgernumbers.ledger_no==ledger_no)).first() is None:
         #     return jsonify({'error':"Invalid Ledger Number."}), HTTP_400_BAD_REQUEST
